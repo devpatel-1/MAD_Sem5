a24473d6b10426de106f7c2cdd608388127d8e3f
Dev Patel <devapatel09092006@gmail.com>
2026-06-25 14:06:56 +0530

    Initial Commit

---
+++ src/P11.kt
@@ -0,0 +1,30 @@
+fun main (){
+    var i : Int = 22
+    println("Integer Value:  "+ i)
+
+    var a : Float = 1.5f
+    println("Float Value: "+ a)
+
+    var b : Char = 'T'
+    println("Char Value: "+ b)
+
+    var c : String = "Dev"
+    println("Character Value: "+ c)
+
+    var d : Boolean = false
+    println("Boolean Value: "+d )
+
+    var e : Double = 96.36
+    println("Double Value: "+ e)
+
+    var f : Long = 7824246823
+    println("Long Value: "+ f)
+
+    var g : Short = -2
+    println("Short Value: "+ g)
+
+    var h : Byte = 127
+    val z = 20000
+    println("Byte Value: "+ h)
+    println("z: "+z)
+}
