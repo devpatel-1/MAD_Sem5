1674d54c3207474ff7bbf16f7f519a268b022e8a
Dev Patel <devapatel09092006@gmail.com>
2026-07-02 13:04:57 +0530

    Practicle - 1 Question 3

---
+++ src/P13.kt
@@ -0,0 +1,46 @@
+fun main () {
+//    var s1: String? = "23"
+//    val a1: Int = s1!!.toInt()
+//    println(a1)
+//
+//    println("Enter Number: ")
+//    var s2 = readln().toInt()
+//    println(s2)
+
+    println("Enter Enrollment Number: ")
+    val eno = readln()
+
+    println("Enter name: ")
+    val name = readln()
+
+    println("Enter Branch: ")
+    val branch = readln()
+
+    println("Enter class: ")
+    val clas = readln()
+
+    println("Enter Batch: ")
+    val batch = readln()
+
+    println("Enter College Name: ")
+    val clg = readln()
+
+    println("Enter University Name: ")
+    val uni = readln()
+
+    println("Enter Age: ")
+    val age = readln().toInt()
+
+    println("Student's Data:")
+    println("Enrollment No.:$eno")
+    println("Name: $name")
+    println("Age: $age")
+    println("Branch: $branch")
+    println("Class: $clas")
+    println("Batch: $batch")
+    println("College Name: $clg")
+    println("University Name: $uni")
+
+
+
+}
